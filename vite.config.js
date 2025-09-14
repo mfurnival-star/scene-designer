cc39e2448d20aebe6130529e556adf01defe3252
Mark Furnival <m.furnival@btinternet.com>
2025-09-14 14:53:07 +0100

    Deploy at 2025-09-14 14:51:45

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// Fixes Golden Layout ESM interop for Vite (see scene-designer Copilot Space)
-export default {
-  optimizeDeps: {
-    include: ['golden-layout']
-  },
-  ssr: {
-    noExternal: ['golden-layout']
-  }
-};
